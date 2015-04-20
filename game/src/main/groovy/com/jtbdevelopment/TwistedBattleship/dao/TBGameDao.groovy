845d64656140c00f5280fd8ca427672fd297e759
JTB <jtbdevelopment@gmail.com>
2015-04-20 19:10:38 -0400

    game dao core bug

--- game/src/main/groovy/com/jtbdevelopment/TwistedBattleship/dao/TBGameDao.groovy
+++ game/src/main/groovy/com/jtbdevelopment/TwistedBattleship/dao/TBGameDao.groovy
@@ -3,7 +3,12 @@ package com.jtbdevelopment.TwistedBattleship.dao
 import com.jtbdevelopment.TwistedBattleship.state.GameFeature
 import com.jtbdevelopment.TwistedBattleship.state.TBGame
 import com.jtbdevelopment.games.mongo.dao.AbstractMongoMultiPlayerGameRepository
+import com.jtbdevelopment.games.state.GamePhase
 import groovy.transform.CompileStatic
+import org.bson.types.ObjectId
+import org.springframework.data.domain.Pageable
+
+import java.time.ZonedDateTime
 
 /**
  * Date: 4/2/15
@@ -11,5 +16,9 @@ import groovy.transform.CompileStatic
  */
 @CompileStatic
 interface TBGameDao extends AbstractMongoMultiPlayerGameRepository<GameFeature, TBGame> {
+    //  TODO - move to core - see comment there
+    //  TODO - also move PlayerGamesFinderHandler then
+    List<TBGame> findByPlayersIdAndGamePhaseAndLastUpdateGreaterThan(
+            final ObjectId id, final GamePhase gamePhase, final ZonedDateTime cutoff, final Pageable pageable)
 
 }
