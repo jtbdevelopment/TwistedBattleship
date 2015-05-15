773db97aa4d7b05a4c3b5b12cb557aa8ed6240ed
JTB <jtbdevelopment@gmail.com>
2015-05-15 07:12:25 -0400

    started adding spy

--- game/src/test/groovy/com/jtbdevelopment/TwistedBattleship/rest/handlers/SpyHandlerTest.groovy
+++ game/src/test/groovy/com/jtbdevelopment/TwistedBattleship/rest/handlers/SpyHandlerTest.groovy
@@ -1,13 +1,16 @@
 package com.jtbdevelopment.TwistedBattleship.rest.handlers
 
+import com.jtbdevelopment.TwistedBattleship.exceptions.NoSpyActionsRemainException
 import com.jtbdevelopment.TwistedBattleship.state.GameFeature
 import com.jtbdevelopment.TwistedBattleship.state.TBGame
+import com.jtbdevelopment.TwistedBattleship.state.TBPlayerState
+import com.jtbdevelopment.games.mongo.MongoGameCoreTestCase
 
 /**
  * Date: 5/15/15
  * Time: 6:56 AM
  */
-class SpyHandlerTest extends GroovyTestCase {
+class SpyHandlerTest extends MongoGameCoreTestCase {
     SpyHandler handler = new SpyHandler()
 
     void testTargetSelf() {
@@ -19,5 +22,18 @@ class SpyHandlerTest extends GroovyTestCase {
         assert 1 == handler.movesRequired(game)
         game.features.add(GameFeature.PerShip)
         assert 2 == handler.movesRequired(game)
+    }
+
+    void testValidatesNoSpiesRemain() {
+        TBGame game = new TBGame(
+                playerDetails: [
+                        (PONE.id): new TBPlayerState(spysRemaining: 1)
+                ]
+        )
+        handler.validateMoveSpecific(PONE, game, PTWO, null)
+        game.playerDetails[PONE.id].spysRemaining = 0
+        shouldFail(NoSpyActionsRemainException.class, {
+            handler.validateMoveSpecific(PONE, game, PTWO, null)
+        })
     }
 }
