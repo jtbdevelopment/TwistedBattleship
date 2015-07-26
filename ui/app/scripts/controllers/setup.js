7b29f3b50dfc6e73872e071876090797e1c5f1c5
JTB <jtbdevelopment@gmail.com>
2015-07-26 19:16:00 -0400

    broke out a lot of phaser code to a service for re-use on playing screen

--- ui/app/scripts/controllers/setup.js
+++ ui/app/scripts/controllers/setup.js
@@ -1,18 +1,15 @@
-/*global Phaser:false */
 'use strict';
 
 var GRID_SIZE = 100;
 var HALF_GRID = GRID_SIZE / 2;
 
 angular.module('tbs.controllers').controller('SetupGameCtrl',
-    ['$scope', 'tbsGameDetails', 'tbsActions', 'jtbGameCache', 'jtbPlayerService', 'jtbFacebook', '$http', '$state', '$location', '$ionicModal', '$ionicSideMenuDelegate', 'tbsShips', // 'twAds',
-        function ($scope, tbsGameDetails, tbsActions, jtbGameCache, jtbPlayerService, jtbFacebook, $http, $state, $location, $ionicModal, $ionicSideMenuDelegate, tbsShips /*, twAds*/) {
+    ['$scope', 'tbsGameDetails', 'tbsActions', 'jtbGameCache', 'jtbPlayerService', 'jtbFacebook', '$http', '$state', '$location', '$ionicModal', '$ionicSideMenuDelegate', 'tbsShips', 'tbsShipGrid', // 'twAds',
+        function ($scope, tbsGameDetails, tbsActions, jtbGameCache, jtbPlayerService, jtbFacebook, $http, $state, $location, $ionicModal, $ionicSideMenuDelegate, tbsShips, tbsShipGrid /*, twAds*/) {
             $ionicSideMenuDelegate.canDragContent(false);
             $scope.theme = 'default';
             $scope.gameID = $state.params.gameID;
             $scope.game = jtbGameCache.getGameForID($scope.gameID);
-            $scope.shipsOnGrid = [];
-            $scope.generalShipInfo = [];
             $scope.gameDetails = tbsGameDetails;
 
             $scope.gameWidth = $scope.game.gridSize * GRID_SIZE;
@@ -21,6 +18,7 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
             $scope.movingShip = null;
             $scope.movingPointerRelativeToShip = {x: 0, y: 0};
             $scope.submitDisabled = true;
+            $scope.shipLocations = [];
 
             $scope.quit = function () {
                 tbsActions.quit($scope);
@@ -28,10 +26,10 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
 
             $scope.submit = function () {
                 var info = {};
-                angular.forEach($scope.shipsOnGrid, function (ship) {
+                angular.forEach(tbsShipGrid.currentShipsOnGrid(), function (ship) {
                     var cells = [];
-                    var startX = ship.startX / GRID_SIZE;
-                    var startY = ship.startY / GRID_SIZE;
+                    var startX = ship.startX / tbsShipGrid.cellSize();
+                    var startY = ship.startY / tbsShipGrid.cellSize();
                     var i = 0;
                     if (ship.horizontal) {
                         for (i = 0; i < ship.info.gridSize; ++i) {
@@ -48,101 +46,22 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
                 tbsActions.setup($scope, info);
             };
 
-            function placeShip(horizontal, row, column, shipInfo) {
-                var centerX, centerY;
-                if (horizontal) {
-                    centerY = (row * GRID_SIZE) + HALF_GRID;
-                    centerX = (column * GRID_SIZE) + (shipInfo.gridSize * HALF_GRID);
-                } else {
-                    centerX = (column * GRID_SIZE) + HALF_GRID;
-                    centerY = (row * GRID_SIZE) + (shipInfo.gridSize * HALF_GRID);
-                }
-                var ship = $scope.phaser.add.sprite(centerX, centerY, shipInfo.ship, 0);
-                ship.anchor.setTo(0.5, 0.5);
-                ship.angle = horizontal ? 0 : 90;
-
-                var shipOnGrid = {
-                    sprite: ship,
-                    info: shipInfo,
-                    horizontal: horizontal
-                };
-                computeShipCorners(shipOnGrid);
-                $scope.shipsOnGrid.push(shipOnGrid);
-            }
-
-            function placeShips() {
-                angular.forEach($scope.game.maskedPlayersState.shipStates, function (value, key) {
-                    var shipInfo = $scope.generalShipInfo.find(function (info) {
-                        return info.ship === key;
-                    });
-                    var horizontal = value.shipGridCells[0].row === value.shipGridCells[1].row;
-                    var row = value.shipGridCells[0].row;
-                    var column = value.shipGridCells[0].column;
-                    placeShip(horizontal, row, column, shipInfo);
-                });
-                if ($scope.shipsOnGrid.length === 0) {
-                    var row = 0;
-                    angular.forEach($scope.generalShipInfo, function (shipInfo) {
-                        placeShip(true, row, 4, shipInfo);
-                        row = row + 1;
-                    });
-                }
-                checkOverlap();
-            }
-
-            function create() {
-                var map = $scope.phaser.add.tilemap('grid');
-                map.addTilesetImage('tile');
-                var layer = map.createLayer('base grid');
-                layer.resizeWorld();
-                placeShips();
-                $scope.phaser.width = $scope.gameWidth * $scope.gameScale;
-                $scope.phaser.height = $scope.gameHeight * $scope.gameScale;
-                $scope.phaser.world.resize($scope.gameWidth * $scope.gameScale, $scope.gameHeight * $scope.gameScale);
-
-                $scope.phaser.input.onTap.add(onTap);
-                $scope.phaser.input.addMoveCallback(onMove);
-                $scope.phaser.input.onDown.add(onDown);
-                $scope.phaser.input.onUp.add(onUp);
-
-                //  TODO  - look at more
-                //$scope.phaser.scale.fullScreenScaleMode = Phaser.ScaleManager.NO_SCALE;
-                //$scope.phaser.scale.startFullScreen(false);
-            }
-
-            function computeShipCorners(shipData) {
-                var halfShip = (GRID_SIZE * shipData.info.gridSize) / 2;
-                shipData.centerX = shipData.sprite.x;
-                shipData.centerY = shipData.sprite.y;
-                if (shipData.horizontal) {
-                    shipData.startX = shipData.centerX - halfShip;
-                    shipData.startY = shipData.centerY - HALF_GRID;
-                    shipData.endX = shipData.centerX + halfShip - 1;
-                    shipData.endY = shipData.centerY + HALF_GRID - 1;
-                } else {
-                    shipData.startY = shipData.centerY - halfShip;
-                    shipData.startX = shipData.centerX - HALF_GRID;
-                    shipData.endY = shipData.centerY + halfShip - 1;
-                    shipData.endX = shipData.centerX + HALF_GRID - 1;
-                }
-            }
-
             function moveOutOfBoundShip(shipData) {
                 if (shipData.startX < 0) {
                     shipData.sprite.x -= shipData.startX;
-                    computeShipCorners(shipData);
+                    tbsShipGrid.computeShipCorners(shipData);
                 }
                 if (shipData.endX >= $scope.gameWidth) {
                     shipData.sprite.x -= (shipData.endX - $scope.gameWidth + 1);
-                    computeShipCorners(shipData);
+                    tbsShipGrid.computeShipCorners(shipData);
                 }
                 if (shipData.startY < 0) {
                     shipData.sprite.y -= shipData.startY;
-                    computeShipCorners(shipData);
+                    tbsShipGrid.computeShipCorners(shipData);
                 }
                 if (shipData.endY >= $scope.gameHeight) {
                     shipData.sprite.y -= (shipData.endY - $scope.gameHeight + 1);
-                    computeShipCorners(shipData);
+                    tbsShipGrid.computeShipCorners(shipData);
                 }
             }
 
@@ -163,21 +82,22 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
                         shipData.sprite.y = shipData.sprite.y - yMod + GRID_SIZE;
                     }
                 }
-                computeShipCorners(shipData);
+                tbsShipGrid.computeShipCorners(shipData);
                 moveOutOfBoundShip(shipData);
             }
 
             function checkOverlap() {
                 var overlapExists = false;
                 var overlaps = [];
-                angular.forEach($scope.shipsOnGrid, function () {
+                var shipsOnGrid = tbsShipGrid.currentShipsOnGrid();
+                angular.forEach(shipsOnGrid, function () {
                     overlaps.push(false);
                 });
 
-                for (var i = 0; i < $scope.shipsOnGrid.length; ++i) {
-                    var outerShip = $scope.shipsOnGrid[i];
-                    for (var j = i + 1; j < $scope.shipsOnGrid.length; ++j) {
-                        var innerShip = $scope.shipsOnGrid[j];
+                for (var i = 0; i < shipsOnGrid.length; ++i) {
+                    var outerShip = shipsOnGrid[i];
+                    for (var j = i + 1; j < shipsOnGrid.length; ++j) {
+                        var innerShip = shipsOnGrid[j];
                         if (outerShip.startX > innerShip.endX ||
                             outerShip.endX < innerShip.startX ||
                             outerShip.startY > innerShip.endY ||
@@ -190,9 +110,9 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
                         overlaps[j] = true;
                     }
                     if (overlaps[i] === true) {
-                        $scope.shipsOnGrid[i].sprite.tint = 0xff0000;
+                        shipsOnGrid[i].sprite.tint = 0xff0000;
                     } else {
-                        $scope.shipsOnGrid[i].sprite.tint = 0xffffff;
+                        shipsOnGrid[i].sprite.tint = 0xffffff;
                     }
                 }
                 $scope.submitDisabled = overlapExists;
@@ -210,43 +130,30 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
 
             function onMove() {
                 if ($scope.movingShip !== null) {
-                    var x = $scope.phaser.input.mousePointer.x / $scope.gameScale;
-                    var y = $scope.phaser.input.mousePointer.y / $scope.gameScale;
-                    $scope.movingShip.sprite.x = x + $scope.movingPointerRelativeToShip.x;
-                    $scope.movingShip.sprite.y = y + $scope.movingPointerRelativeToShip.y;
-                    computeShipCorners($scope.movingShip);
+                    var coords = tbsShipGrid.currentMouseCoordinates();
+                    $scope.movingShip.sprite.x = coords.x + $scope.movingPointerRelativeToShip.x;
+                    $scope.movingShip.sprite.y = coords.y + $scope.movingPointerRelativeToShip.y;
+                    tbsShipGrid.computeShipCorners($scope.movingShip);
                 }
-            }
-
-            function findShipByAdjustedCoordinates(x, y) {
-                for (var i = 0; i < $scope.shipsOnGrid.length; ++i) {
-                    if (x >= $scope.shipsOnGrid[i].startX && y >= $scope.shipsOnGrid[i].startY && x < $scope.shipsOnGrid[i].endX && y < $scope.shipsOnGrid[i].endY) {
-                        return $scope.shipsOnGrid[i];
-                    }
-                }
-                return null;
             }
 
             function onDown() {
-                var x = $scope.phaser.input.mousePointer.x / $scope.gameScale;
-                var y = $scope.phaser.input.mousePointer.y / $scope.gameScale;
-                $scope.movingShip = findShipByAdjustedCoordinates(x, y);
+                var coords = tbsShipGrid.currentMouseCoordinates();
+                $scope.movingShip = tbsShipGrid.findShipByMouseCoordinates();
                 if ($scope.movingShip !== null) {
-                    $scope.movingPointerRelativeToShip.x = $scope.movingShip.centerX - x;
-                    $scope.movingPointerRelativeToShip.y = $scope.movingShip.centerY - y;
+                    $scope.movingPointerRelativeToShip.x = $scope.movingShip.centerX - coords.x;
+                    $scope.movingPointerRelativeToShip.y = $scope.movingShip.centerY - coords.y;
                     $scope.movingShip.sprite.tint = 0x00ff00;
                 }
             }
 
             function onTap(pointer, double) {
                 if (double) {
-                    var x = $scope.phaser.input.mousePointer.x / $scope.gameScale;
-                    var y = $scope.phaser.input.mousePointer.y / $scope.gameScale;
-                    var ship = findShipByAdjustedCoordinates(x, y);
+                    var ship = tbsShipGrid.findShipByMouseCoordinates();
 
                     if (ship) {
                         ship.horizontal = !(ship.horizontal);
-                        computeShipCorners(ship);
+                        tbsShipGrid.computeShipCorners(ship);
                         roundPosition(ship);
                         checkOverlap();
                         ship.sprite.angle = ship.horizontal ? 0 : 90;
@@ -254,28 +161,36 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
                 }
             }
 
-            function preload() {
-                var json = $scope.gameHeight === 1000 ? '10x10.json' : $scope.gameHeight === 2000 ? '20x20.json' : '15x15.json';
-                $scope.phaser.load.tilemap('grid', '/templates/gamefiles/' + json, null, Phaser.Tilemap.TILED_JSON);
-                $scope.phaser.load.image('tile', '/images/default/tile.png');
-                $scope.phaser.load.image('Destroyer', '/images/default/destroyer.png');
-                $scope.phaser.load.image('Submarine', '/images/default/submarine.png');
-
-                // TODO
-                $scope.phaser.load.image('Carrier', '/images/default/destroyer.png');
-                $scope.phaser.load.image('Battleship', '/images/default/destroyer.png');
-                $scope.phaser.load.image('Cruiser', '/images/default/destroyer.png');
+            function computeShipLocations(generalShipInfo) {
+                var shipLocations = [];
+                angular.forEach($scope.game.maskedPlayersState.shipStates, function (value, key) {
+                    var shipInfo = generalShipInfo.find(function (info) {
+                        return info.ship === key;
+                    });
+                    var horizontal = value.shipGridCells[0].row === value.shipGridCells[1].row;
+                    var row = value.shipGridCells[0].row;
+                    var column = value.shipGridCells[0].column;
+                    shipLocations.push({horizontal: horizontal, row: row, column: column, shipInfo: shipInfo});
+                });
+                if (shipLocations.length === 0) {
+                    var row = 0;
+                    angular.forEach(generalShipInfo, function (shipInfo) {
+                        shipLocations.push({horizontal: true, row: row, column: 4, shipInfo: shipInfo});
+                        row = row + 1;
+                    });
+                }
+                return shipLocations;
             }
 
             tbsShips.ships().then(
-                function (ships) {
-                    $scope.generalShipInfo = ships;
-                    $scope.phaser = new Phaser.Game(
-                        $scope.gameWidth,
-                        $scope.gameHeight,
-                        Phaser.AUTO,
-                        'phaser',
-                        {preload: preload, create: create});
+                function (generalShipInfo) {
+                    tbsShipGrid.initialize($scope.game, computeShipLocations(generalShipInfo), function () {
+                        tbsShipGrid.onDown(onDown);
+                        tbsShipGrid.onMove(onMove);
+                        tbsShipGrid.onTap(onTap);
+                        tbsShipGrid.onUp(onUp);
+                        checkOverlap();
+                    });
                 },
                 function () {
                     //  TODO
---
+++ ui/app/scripts/services/shipGrid.js
@@ -0,0 +1,176 @@
+/*global Phaser:false */
+'use strict';
+
+angular.module('tbs.services').factory('tbsShipGrid',
+    [
+        function () {
+            var CELL_SIZE = 100;
+            var HALF_CELL_SIZE = CELL_SIZE / 2;
+
+            var theme;
+            var game;
+            var shipsOnGrid, shipLocations;
+            var gameWidth, gameHeight, gameScale;
+            var phaser;
+            var postCreateFunction;
+
+            function preload() {
+                var json = gameHeight === 1000 ? '10x10.json' : gameHeight === 2000 ? '20x20.json' : '15x15.json';
+                phaser.load.tilemap('grid', '/templates/gamefiles/' + json, null, Phaser.Tilemap.TILED_JSON);
+                phaser.load.image('tile', '/images/default/tile.png');
+                phaser.load.image('Destroyer', '/images/default/destroyer.png');
+                phaser.load.image('Submarine', '/images/default/submarine.png');
+
+                // TODO
+                phaser.load.image('Carrier', '/images/default/destroyer.png');
+                phaser.load.image('Battleship', '/images/default/destroyer.png');
+                phaser.load.image('Cruiser', '/images/default/destroyer.png');
+            }
+
+            function create() {
+                var map = phaser.add.tilemap('grid');
+                map.addTilesetImage('tile');
+                var layer = map.createLayer('base grid');
+                layer.resizeWorld();
+                phaser.width = gameWidth * gameScale;
+                phaser.height = gameHeight * gameScale;
+                phaser.world.resize(gameWidth * gameScale, gameHeight * gameScale);
+
+                placeShips();
+
+                //  TODO  - look at more
+                //phaser.scale.fullScreenScaleMode = Phaser.ScaleManager.NO_SCALE;
+                //phaser.scale.startFullScreen(false);
+
+                if (postCreateFunction !== null) {
+                    postCreateFunction();
+                }
+            }
+
+            function computeShipCorners(shipData) {
+                var halfShip = (CELL_SIZE * shipData.info.gridSize) / 2;
+                shipData.centerX = shipData.sprite.x;
+                shipData.centerY = shipData.sprite.y;
+                if (shipData.horizontal) {
+                    shipData.startX = shipData.centerX - halfShip;
+                    shipData.startY = shipData.centerY - HALF_CELL_SIZE;
+                    shipData.endX = shipData.centerX + halfShip - 1;
+                    shipData.endY = shipData.centerY + HALF_CELL_SIZE - 1;
+                } else {
+                    shipData.startY = shipData.centerY - halfShip;
+                    shipData.startX = shipData.centerX - HALF_CELL_SIZE;
+                    shipData.endY = shipData.centerY + halfShip - 1;
+                    shipData.endX = shipData.centerX + HALF_CELL_SIZE - 1;
+                }
+            }
+
+            function placeShip(horizontal, row, column, shipInfo) {
+                var centerX, centerY;
+                if (horizontal) {
+                    centerY = (row * CELL_SIZE) + HALF_CELL_SIZE;
+                    centerX = (column * CELL_SIZE) + (shipInfo.gridSize * HALF_CELL_SIZE);
+                } else {
+                    centerX = (column * CELL_SIZE) + HALF_CELL_SIZE;
+                    centerY = (row * CELL_SIZE) + (shipInfo.gridSize * HALF_CELL_SIZE);
+                }
+                var ship = phaser.add.sprite(centerX, centerY, shipInfo.ship, 0);
+                ship.anchor.setTo(0.5, 0.5);
+                ship.angle = horizontal ? 0 : 90;
+
+                var shipOnGrid = {
+                    sprite: ship,
+                    info: shipInfo,
+                    horizontal: horizontal
+                };
+                computeShipCorners(shipOnGrid);
+                shipsOnGrid.push(shipOnGrid);
+            }
+
+            function placeShips() {
+                shipsOnGrid = [];
+                angular.forEach(shipLocations, function (shipLocation) {
+                    placeShip(shipLocation.horizontal, shipLocation.row, shipLocation.column, shipLocation.shipInfo);
+                });
+            }
+
+            return {
+                initialize: function (loadedGame, initialShipLocations, postCreateCB) {
+                    theme = 'default';
+                    game = loadedGame;
+                    gameWidth = game.gridSize * CELL_SIZE;
+                    gameHeight = game.gridSize * CELL_SIZE;
+                    gameScale = 0.5;
+                    shipLocations = initialShipLocations;
+                    postCreateFunction = postCreateCB;
+                    phaser = new Phaser.Game(
+                        gameWidth,
+                        gameHeight,
+                        Phaser.AUTO,
+                        'phaser',
+                        {preload: preload, create: create});
+                },
+
+                placeShips: function (initialShipLocations) {
+                    shipLocations = initialShipLocations;
+                    placeShips();
+                },
+
+                currentShipsOnGrid: function () {
+                    return shipsOnGrid;
+                },
+
+                gameWidth: function () {
+                    return gameWidth;
+                },
+                gameHeight: function () {
+                    return gameHeight;
+                },
+                cellSize: function () {
+                    return CELL_SIZE;
+                },
+                computeShipCorners: function (shipData) {
+                    computeShipCorners(shipData);
+                },
+
+                currentMouseCoordinates: function () {
+                    var x = phaser.input.mousePointer.x / gameScale;
+                    var y = phaser.input.mousePointer.y / gameScale;
+                    return {x: x, y: y};
+                },
+                findShipByMouseCoordinates: function () {
+                    var coords = this.currentMouseCoordinates();
+                    for (var i = 0; i < shipsOnGrid.length; ++i) {
+                        var shipOnGrid = shipsOnGrid[i];
+                        if (coords.x >= shipOnGrid.startX &&
+                            coords.y >= shipOnGrid.startY &&
+                            coords.x < shipOnGrid.endX &&
+                            coords.y < shipOnGrid.endY) {
+                            return shipOnGrid;
+                        }
+                    }
+                    return null;
+                },
+
+                phaser: function () {
+                    return phaser;
+                },
+
+                onMove: function (cb) {
+                    phaser.input.addMoveCallback(cb);
+                },
+
+                onDown: function (cb) {
+                    phaser.input.onDown.add(cb);
+                },
+
+                onUp: function (cb) {
+                    phaser.input.onUp.add(cb);
+                },
+
+                onTap: function (cb) {
+                    phaser.input.onTap.add(cb);
+                }
+            };
+        }
+    ]
+);
