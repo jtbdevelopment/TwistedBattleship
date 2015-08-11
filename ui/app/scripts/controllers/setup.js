f6429ce90f6cd2df8f90c2c0569be96d1ff1a8a3
JTB <jtbdevelopment@gmail.com>
2015-08-11 19:20:01 -0400

    styled setup and added setup help.

--- ui/app/scripts/controllers/setup.js
+++ ui/app/scripts/controllers/setup.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('tbs.controllers').controller('SetupGameCtrl',
-    ['$scope', 'tbsGameDetails', 'tbsActions', 'jtbGameCache', 'jtbPlayerService', '$state', '$ionicSideMenuDelegate', 'tbsShips', 'tbsShipGrid', // 'twAds',
-        function ($scope, tbsGameDetails, tbsActions, jtbGameCache, jtbPlayerService, $state, $ionicSideMenuDelegate, tbsShips, tbsShipGrid /*, twAds*/) {
+    ['$scope', 'tbsGameDetails', 'tbsActions', 'jtbGameCache', 'jtbPlayerService', '$state', '$ionicSideMenuDelegate', 'tbsShips', 'tbsShipGrid', '$ionicModal', // 'twAds',
+        function ($scope, tbsGameDetails, tbsActions, jtbGameCache, jtbPlayerService, $state, $ionicSideMenuDelegate, tbsShips, tbsShipGrid, $ionicModal /*, twAds*/) {
             $ionicSideMenuDelegate.canDragContent(false);
             $scope.theme = 'default';
             $scope.gameID = $state.params.gameID;
@@ -12,6 +12,51 @@ angular.module('tbs.controllers').controller('SetupGameCtrl',
             $scope.movingShip = null;
             $scope.movingPointerRelativeToShip = {x: 0, y: 0};
             $scope.submitDisabled = true;
+
+            $scope.helpTopics = [
+                {
+                    topic: 'General Help',
+                    description: 'This is where you place your own ships.  Once you are happy, press Save.  The game begins when all players have saved their ships.  As long as not all players have saved their setup, you can keep moving your ships.'
+                },
+                {
+                    topic: 'Moving Ships',
+                    description: 'You can drag and drop your ships into position.  Ships cannot be placed on top of each other or out of bounds.  Double clicking or double tapping a ship will rotate it 90 degrees.  If ships are red, it means they are on top of each other and need to be moved.'
+                },
+            ];
+            $ionicModal.fromTemplateUrl('help-modal.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function (modal) {
+                $scope.helpModal = modal;
+                $scope.helpIndex = 0;
+            });
+
+            $scope.previousHelp = function () {
+                $scope.helpIndex--;
+                if ($scope.helpIndex < 0) {
+                    $scope.helpIndex = $scope.helpTopics.length - 1;
+                }
+            };
+
+            $scope.nextHelp = function () {
+                $scope.helpIndex++;
+                if ($scope.helpIndex === $scope.helpTopics.length) {
+                    $scope.helpIndex = 0;
+                }
+            };
+
+            $scope.showHelp = function () {
+                $scope.helpIndex = 0;
+                $scope.helpModal.show();
+            };
+
+            $scope.closeHelp = function () {
+                $scope.helpModal.hide();
+            };
+
+            $scope.$on('$destroy', function () {
+                $scope.helpModal.remove();
+            });
 
             $scope.quit = function () {
                 tbsActions.quit($scope);
