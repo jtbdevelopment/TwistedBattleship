fbf8e62f1655face531b34e17b83506d27d3cd61
JTB <jtbdevelopment@gmail.com>
2015-09-07 12:07:07 -0400

    added browser platform to see how that works for later

--- ui/hooks/after_prepare/update_platform_config.js
+++ ui/hooks/after_prepare/update_platform_config.js
@@ -125,7 +125,8 @@ var platformConfig = (function () {
                 destination: 'android:windowSoftInputMode'
             }
         },
-        'ios': {}
+        'ios': {},
+        'browser': {}
     };
     var configXmlData, preferencesData;
 
