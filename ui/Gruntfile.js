7801f93d44a07fdfef9a48bbf2fa4c30a5d011f1
JTB <jtbdevelopment@gmail.com>
2015-09-09 22:36:14 -0400

    more options in dev

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -44,7 +44,10 @@ module.exports = function (grunt) {
                     ENV: {
                         name: 'development',
                         domain: 'localhost',
-                        apiEndpoint: 'http://localhost:9998'
+                        apiEndpointOther: 'http://localhost:9998',
+                        apiEndpointAndroidEmulator: 'http://10.0.2.2:9998',
+                        apiEndpointGenymotionEmulator: 'http://192.168.56.1:9998',
+                        apiEndpoint: 'http://192.168.56.1:9998'
                     }
                 }
             },
