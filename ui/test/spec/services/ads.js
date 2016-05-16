6ef0e14035d4531ea2e44d9745301e378b45f1e2
JTB <jtbdevelopment@gmail.com>
2016-05-16 06:43:39 -0400

    partial ads test

--- ui/test/spec/services/ads.js
+++ ui/test/spec/services/ads.js
@@ -1,6 +1,22 @@
 /*global AdMob:false */
 'use strict';
 
+window.AdMob = {
+    AD_POSITION: {
+        NO_CHANGE: 0,
+        TOP_LEFT: 1,
+        TOP_CENTER: 2,
+        TOP_RIGHT: 3,
+        LEFT: 4,
+        CENTER: 5,
+        RIGHT: 6,
+        BOTTOM_LEFT: 7,
+        BOTTOM_CENTER: 8,
+        BOTTOM_RIGHT: 9,
+        POS_XY: 10
+    }
+};
+
 describe('Service: ads', function () {
     // load the controller's module
     beforeEach(module('tbs.services'));
@@ -65,50 +81,61 @@ describe('Service: ads', function () {
         });
     });
 
-    describe('for ios platform', function () {
-        beforeEach(function () {
-            platformUnderTest = 'iOS';
-        });
-
-        it('initializes correctly', function () {
-            service.initialize();
-            expect(googleAdSpy.prepareInterstitial.callCount).to.equal(1);
-            assert(googleAdSpy.prepareInterstitial.calledWithMatch({
-                adId: 'ca-app-pub-8812482609918940/9839986116',
-                autoShow: false
-            }));
-            assert(googleAdSpy.createBanner.calledWithMatch({
-                adId: 'ca-app-pub-8812482609918940/3876007710',
-                position: AdMob.AD_POSITION.BOTTOM_CENTER,
-                autoShow: true
-            }));
-        });
-
-        describe('calling ads', function () {
+    var platformsToTest = {
+        iOS: ['ca-app-pub-8812482609918940/9839986116', 'ca-app-pub-8812482609918940/2316719315'],
+        Android: ['ca-app-pub-8812482609918940/5352740910', 'ca-app-pub-8812482609918940/3876007710']
+    };
+    angular.forEach(platformsToTest, function (keys, platform) {
+        describe('for ' + platform + ' platform', function () {
             beforeEach(function () {
-                service.initialize();
+                platformUnderTest = platform;
             });
 
-            /*
-             it('show interstitial first time', function() {
-             window.invokeApplixirVideoUnitExtended = sinon.spy();
-             service.showInterstitial();
-             // can't test callback function
-             assert(window.invokeApplixirVideoUnitExtended.calledWith(false, 'middle'));
-             expect(window.invokeApplixirVideoUnitExtended.callCount).to.equal(1);
-             });
+            it('initializes correctly', function () {
+                service.initialize();
+                expect(googleAdSpy.prepareInterstitial.callCount).to.equal(1);
+                assert(googleAdSpy.prepareInterstitial.calledWithMatch({
+                    adId: keys[0],
+                    autoShow: false
+                }));
+                expect(googleAdSpy.createBanner.callCount).to.equal(1);
+                assert(googleAdSpy.createBanner.calledWithMatch({
+                    adId: keys[1],
+                    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+                    autoShow: true
+                }));
+            });
 
-             it('show interstitial rapidly after first time does nothing second time', function() {
-             window.invokeApplixirVideoUnitExtended = sinon.spy();
-             service.showInterstitial();
-             // can't test callback function
-             assert(window.invokeApplixirVideoUnitExtended.calledWith(false, 'middle'));
-             expect(window.invokeApplixirVideoUnitExtended.callCount).to.equal(1);
-             window.invokeApplixirVideoUnitExtended.getCall(0).args[2]();
-             service.showInterstitial();
-             expect(window.invokeApplixirVideoUnitExtended.callCount).to.equal(1);
-             });
-             */
+            describe('calling ads', function () {
+                beforeEach(function () {
+                    service.initialize();
+                });
+
+                it('show interstitial first time', function () {
+                    window.invokeApplixirVideoUnitExtended = sinon.spy();
+                    service.showInterstitial();
+                    expect(googleAdSpy.showInterstitial.callCount).to.equal(1);
+                    assert(googleAdSpy.showInterstitial.calledWithMatch());
+                });
+
+                //  TODO - figure out firing custom events under phantom/karma
+                /*
+                 it('show interstitial rapidly after first time does nothing second time', function () {
+                 window.invokeApplixirVideoUnitExtended = sinon.spy();
+                 service.showInterstitial();
+                 var event = new CustomEvent('build', {adType: 'interstitial'});
+                 console.log(JSON.stringify(event));
+                 event.initEvent('AdDismiss', true, false);
+                 console.log(JSON.stringify(event));
+                 event.adType = 'interstitial';
+                 console.log(JSON.stringify(event));
+                 expect(googleAdSpy.showInterstitial.callCount).to.equal(1);
+                 assert(googleAdSpy.showInterstitial.calledWithMatch());
+                 service.showInterstitial();
+                 expect(googleAdSpy.showInterstitial.callCount).to.equal(1);
+                 });
+                 */
+            });
         });
     });
 });
