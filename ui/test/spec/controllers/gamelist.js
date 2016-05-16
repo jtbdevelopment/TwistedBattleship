277f6167b7e003a1fafad3e3c0cd9c0a1f127d02
JTB <jtbdevelopment@gmail.com>
2016-05-16 18:59:55 -0400

    finished gamelist

--- ui/test/spec/controllers/gamelist.js
+++ ui/test/spec/controllers/gamelist.js
@@ -86,4 +86,45 @@ describe('Controller: MobileGameListCtrl', function () {
         scope.switchHideGames(expectedClassifications[2]);
         expect(scope.games[expectedClassifications[2]].hideGames).to.be.false;
     });
+
+    it('refresh broadcasts to game cache', function () {
+        rootScope.$broadcast = sinon.spy();
+        scope.refreshGames();
+        expect(rootScope.$broadcast.callCount).to.equal(1);
+        assert(rootScope.$broadcast.calledWithMatch('refreshGames', ''));
+    });
+
+    it('new game action goes to setup', function () {
+        scope.createNew();
+        assert(stateSpy.go.calledWithMatch('app.create'));
+    });
+
+    describe('controller loaded after cache loaded', function () {
+        beforeEach(inject(function ($rootScope, $controller) {
+            expectedInitialized = true;
+            scope = rootScope.$new();
+            ctrl = $controller('MobileGameListCtrl', {
+                $scope: scope,
+                $state: stateSpy,
+                $rootScope: rootScope,
+                jtbGameClassifier: mockClassifier,
+                jtbGameCache: mockGameCache,
+                jtbPlayerService: mockPlayerService,
+                tbsGameDetails: mockGameDetails
+            });
+        }));
+
+        it('initializes games immediately', function () {
+            angular.forEach(expectedClassifications, function (classification) {
+                expect(scope.games[classification].games).to.deep.equal(expectedGames[classification]);
+                expect(scope.games[classification].icon).to.equal(expectedIcons[classification]);
+                expect(scope.games[classification].hideGames).to.be.false;
+                expect(scope.games[classification].label).to.equal(classification);
+            });
+            expect(scope.md5).to.equal(currentPlayer.md5);
+            expect(scope.phasesInOrder).to.deep.equal(expectedClassifications);
+            expect(scope.gameDetails).to.equal(mockGameDetails);
+        });
+    });
+
 });
