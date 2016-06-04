18cdace4d4863799af891a344680a57a119dafbc
JTB <jtbdevelopment@gmail.com>
2016-06-03 22:07:11 -0400

    started revamping ship grid service

---
+++ ui/app/scripts/services/shipGridV2.js
@@ -0,0 +1,353 @@
+'use strict';
+
+//  TODO - testing - Avoiding tests - plan to rewrite a fair bit to use a lot more native phaser functionality
+//  for drag/doubletap/compute overlap
+angular.module('tbs.services').factory('tbsShipGridV2',
+    ['jtbPlayerService', 'tbsCircles', 'tbsCellStates', 'tbsShips', 'Phaser',
+        function (jtbPlayerService, tbsCircles, tbsCellStates, tbsShips, Phaser) {
+            var CELL_SIZE = 100;
+            var HALF_CELL_SIZE = CELL_SIZE / 2;
+
+            var cellStateMap;
+            var cellMarkersToPlace = [], cellMarkersOnGrid = [];
+
+            var circleDataFromServers, circleCombinedRelativeCoordinates;
+            var circleSprites = [];
+
+            var shipNames = [], shipInfoMap = {};
+            var shipsOnGrid = [], shipStatesToPlace = [];
+
+            var highlightSprite = null, highlightedShip = null;
+            var highlightedX, highlightedY;
+
+            var theme;
+
+            var gameWidth, gameHeight;
+            var phaser;
+
+            var postCreateCallback;
+            var highlightCallback;
+
+            function preload() {
+                circleCombinedRelativeCoordinates = [];
+                var json;
+                switch (gameHeight) {
+                    case 1000:
+                        json = '10x10.json';
+                        angular.forEach(circleDataFromServers['10'], function (coord) {
+                            circleCombinedRelativeCoordinates.push(coord);
+                        });
+                        break;
+                    case 1500:
+                        angular.forEach(circleDataFromServers['10'], function (coord) {
+                            circleCombinedRelativeCoordinates.push(coord);
+                        });
+                        angular.forEach(circleDataFromServers['15'], function (coord) {
+                            circleCombinedRelativeCoordinates.push(coord);
+                        });
+                        json = '15x15.json';
+                        break;
+                    case 2000:
+                        angular.forEach(circleDataFromServers['10'], function (coord) {
+                            circleCombinedRelativeCoordinates.push(coord);
+                        });
+                        angular.forEach(circleDataFromServers['15'], function (coord) {
+                            circleCombinedRelativeCoordinates.push(coord);
+                        });
+                        angular.forEach(circleDataFromServers['20'], function (coord) {
+                            circleCombinedRelativeCoordinates.push(coord);
+                        });
+                        json = '20x20.json';
+                        break;
+                }
+
+                //  Get rid of 0,0 so it can be highlighted explicitly
+                circleCombinedRelativeCoordinates.splice(0, 1);
+
+                angular.forEach(cellMarkersOnGrid, function (cellMarkerOnGrid) {
+                    cellMarkerOnGrid.destroy();
+                });
+                cellMarkersOnGrid = [];
+
+                angular.forEach(circleSprites, function (circleSprite) {
+                    circleSprite.destroy();
+                });
+                circleSprites = [];
+
+                if (highlightSprite !== null) {
+                    highlightSprite.destroy();
+                }
+                highlightSprite = null;
+                highlightedX = -1;
+                highlightedY = -1;
+
+                angular.forEach(shipsOnGrid, function (shipOnGrid) {
+                    shipOnGrid.destroy();
+                });
+                shipsOnGrid = [];
+
+                highlightCallback = null;
+                //  TODO - see if we can have one tilemap file
+                phaser.load.tilemap('grid', 'templates/gamefiles/' + json, null, Phaser.Tilemap.TILED_JSON);
+                phaser.load.image('tile', 'images/' + theme + '/tile.png');
+                phaser.load.image('highlight', 'images/' + theme + '/highlight.png');
+                phaser.load.image('extendedhighlight', 'images/' + theme + '/extendedhighlight.png');
+                angular.forEach(cellStateMap, function (state) {
+                    var lower = angular.lowercase(state);
+                    phaser.load.image(lower, 'images/' + theme + '/' + lower + '.png');
+                });
+                angular.forEach(shipNames, function (shipName) {
+                    phaser.load.image(shipName, 'images/' + theme + '/' + angular.lowercase(shipName) + '.png');
+                });
+            }
+
+            function init() {
+                phaser.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            }
+
+            function create() {
+                var map = phaser.add.tilemap('grid');
+                map.addTilesetImage('tile');
+                map.createLayer('base grid');
+
+                phaser.scale.pageAlignHorizontally = false;
+                phaser.scale.pageAlignVertically = false;
+                phaser.scale.windowConstraints.bottom = 'visual';
+
+                refreshShipsOnGrid();
+                refreshCellMarkersOnGrid();
+
+                if (postCreateCallback !== null) {
+                    postCreateCallback();
+                }
+            }
+
+            function refreshCellMarkersOnGrid() {
+                angular.forEach(cellMarkersOnGrid, function (markersOnGridRow) {
+                    angular.forEach(markersOnGridRow, function (markersOnGridCell) {
+                        markersOnGridCell.destroy();
+                    });
+                });
+                cellMarkersOnGrid = [];
+                var row = 0;
+                angular.forEach(cellMarkersToPlace, function (cellMarkerRow) {
+                    var rowArray = [];
+                    cellMarkersOnGrid.push(rowArray);
+                    var col = 0;
+                    angular.forEach(cellMarkerRow, function (cellMarkerCell) {
+                        var type = angular.lowercase(cellMarkerCell);
+                        var marker = phaser.add.sprite(col * CELL_SIZE, row * CELL_SIZE, type, 0);
+                        rowArray.push(marker);
+                        ++col;
+                    });
+                    ++row;
+                });
+            }
+
+            //  Takes shipState from server
+            function placeShip(shipState) {
+                var centerX, centerY;
+                var firstCell = shipState.shipGridCells[0];
+                var shipInfo = shipInfoMap[shipState.ship];
+                if (shipState.horizontal) {
+                    centerY = (firstCell.row * CELL_SIZE) + HALF_CELL_SIZE;
+                    centerX = (firstCell.column * CELL_SIZE) + (shipInfo.gridSize * HALF_CELL_SIZE);
+                } else {
+                    centerX = (firstCell.column * CELL_SIZE) + HALF_CELL_SIZE;
+                    centerY = (firstCell.row * CELL_SIZE) + (shipInfo.gridSize * HALF_CELL_SIZE);
+                }
+                var ship = phaser.add.sprite(centerX, centerY, shipState.ship, 0);
+                ship.anchor.setTo(0.5, 0.5);
+                ship.angle = shipState.horizontal ? 0 : 90;
+
+                var shipOnGrid = {
+                    sprite: ship,
+                    shipState: shipState,
+                    info: shipInfo
+                };
+                shipsOnGrid.push(shipOnGrid);
+            }
+
+            /*
+             function currentContextCoordinates(context) {
+             var x = context.worldX;
+             var y = context.worldY;
+             return {x: x, y: y};
+             }
+
+             function highlightAShip(x, y) {
+             if (highlightedShip !== null) {
+             highlightedShip.sprite.tint = 0xffffff;
+             }
+             highlightedShip = findShipByCoordinates({x: x, y: y});
+             if (highlightedShip !== null) {
+             highlightedShip.sprite.tint = 0x00ff00;
+             }
+             }
+
+             function highlightCell(context) {
+             var coords = currentContextCoordinates(context);
+             var y = Math.floor(coords.y / CELL_SIZE) * CELL_SIZE;
+             var x = Math.floor(coords.x / CELL_SIZE) * CELL_SIZE;
+             if (highlightSprite === null) {
+             highlightSprite = phaser.add.sprite(x, y, 'highlight', 0);
+             angular.forEach(circleCombinedRelativeCoordinates, function () {
+             circleSprites.push(phaser.add.sprite(x, y, 'extendedhighlight', 0));
+             });
+             }
+             highlightSprite.x = x;
+             highlightSprite.y = y;
+             highlightedX = x / CELL_SIZE;
+             highlightedY = y / CELL_SIZE;
+             for (var i = 0; i < circleCombinedRelativeCoordinates.length; ++i) {
+             var coord = circleCombinedRelativeCoordinates[i];
+             var sprite = circleSprites[i];
+             sprite.x = x + (coord.column * CELL_SIZE);
+             sprite.y = y + (coord.row * CELL_SIZE);
+             }
+             highlightAShip(x, y);
+             if (highlightCallback !== null) {
+             highlightCallback();
+             }
+             }
+
+             function findShipByCoordinates(coords) {
+             for (var i = 0; i < shipsOnGrid.length; ++i) {
+             var shipOnGrid = shipsOnGrid[i];
+             if (coords.x >= shipOnGrid.startX &&
+             coords.y >= shipOnGrid.startY &&
+             coords.x < shipOnGrid.endX &&
+             coords.y < shipOnGrid.endY) {
+             return shipOnGrid;
+             }
+             }
+             return null;
+             }
+             */
+
+            function refreshShipsOnGrid() {
+                angular.forEach(shipsOnGrid, function (ship) {
+                    ship.sprite.destroy();
+                });
+                shipsOnGrid = [];
+                angular.forEach(shipStatesToPlace, function (shipState) {
+                    placeShip(shipState);
+                });
+            }
+
+            return {
+                stop: function () {
+                    if (angular.isDefined(phaser)) {
+                        phaser.destroy();
+                        phaser = null;
+                    }
+                },
+                initialize: function (loadedGame, initialShipStates, initialCellMarkers, postCreateCB) {
+                    theme = jtbPlayerService.currentPlayer().gameSpecificPlayerAttributes.theme;
+                    gameWidth = loadedGame.gridSize * CELL_SIZE;
+                    gameHeight = loadedGame.gridSize * CELL_SIZE;
+                    shipStatesToPlace = initialShipStates;
+                    cellMarkersToPlace = initialCellMarkers;
+                    postCreateCallback = postCreateCB;
+                    tbsShips.ships().then(function (shipData) {
+                        shipNames = [];
+                        shipInfoMap = {};
+                        angular.forEach(shipData, function (singleShip) {
+                            shipNames.push(singleShip.ship);
+                            shipInfoMap[singleShip.ship] = singleShip;
+                        });
+                        tbsCellStates.cellStates().then(function (stateData) {
+                            cellStateMap = stateData;
+                            tbsCircles.circles().then(
+                                function (circleData) {
+                                    circleDataFromServers = circleData;
+                                    phaser = new Phaser.Game(
+                                        gameWidth,
+                                        gameHeight,
+                                        Phaser.AUTO,
+                                        'phaser',
+                                        {preload: preload, create: create, init: init});
+                                },
+                                function (error) {
+                                    //  TODO
+                                    console.warn(error);
+                                }
+                            );
+                        });
+                    });
+                },
+
+                placeShips: function (newShipStates) {
+                    shipStatesToPlace = newShipStates;
+                    refreshShipsOnGrid();
+                    highlightAShip(highlightedX * CELL_SIZE, highlightedY * CELL_SIZE);
+                },
+
+                placeCellMarkers: function (newCellMarkers) {
+                    cellMarkersToPlace = newCellMarkers;
+                    refreshCellMarkersOnGrid();
+                },
+
+                currentShipsOnGrid: function () {
+                    return shipsOnGrid;
+                },
+                halfCellSize: function () {
+                    return HALF_CELL_SIZE;
+                },
+
+                /*
+                 gameWidth: function () {
+                 return gameWidth;
+                 },
+                 gameHeight: function () {
+                 return gameHeight;
+                 },
+                 cellSize: function () {
+                 return CELL_SIZE;
+                 },
+
+                 activateHighlighting: function (highlightCB) {
+                 highlightCallback = highlightCB;
+                 this.onDown(highlightCell);
+                 },
+                 selectedCell: function () {
+                 return {x: highlightedX, y: highlightedY};
+                 },
+                 selectedShip: function () {
+                 return findShipByCoordinates({x: highlightedX * CELL_SIZE, y: highlightedY * CELL_SIZE});
+                 },
+
+                 computeShipCorners: function (shipData) {
+                 computeShipCorners(shipData);
+                 },
+
+                 currentContextCoordinates: function (context) {
+                 return currentContextCoordinates(context);
+                 },
+
+                 findShipByContextCoordinates: function (context) {
+                 return findShipByCoordinates(currentContextCoordinates(context));
+                 },
+
+                 onMove: function (cb) {
+                 phaser.input.addMoveCallback(cb);
+                 },
+
+                 onDown: function (cb) {
+                 phaser.input.onDown.add(cb);
+                 },
+
+                 onUp: function (cb) {
+                 phaser.input.onUp.add(cb);
+                 },
+
+                 onTap: function (cb) {
+                 phaser.input.onTap.add(cb);
+                 }
+                 */
+                placeholder: function () {
+                }
+            };
+        }
+    ]
+);
