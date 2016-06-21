e71ddc6db7a747f87c4ca6a0a09988cfc6526f96
JTB <jtbdevelopment@gmail.com>
2016-06-21 07:11:28 -0400

    initial ship grid testing - broke out new Phaser.Game to factory to aid in testing

--- ui/test/spec/services/shipGridV2.js
+++ ui/test/spec/services/shipGridV2.js
@@ -4,7 +4,7 @@ describe('Service: gameDetails', function () {
     // load the controller's module
     beforeEach(module('tbs.services'));
 
-    var expectedTheme = 'A THEME!';
+    var expectedTheme = 'mytheme';
     var currentPlayer = {source: 'MANUAL', gameSpecificPlayerAttributes: {theme: expectedTheme}};
     var expectedGame = {gridSize: 20};
 
@@ -42,8 +42,35 @@ describe('Service: gameDetails', function () {
             'row': -1, 'column': -2
         }, {'row': -2, 'column': 1}, {'row': 1, 'column': -2}]
     };
-    var Phaser = {};
-    var PhaserGame = {}, PhaserFactory = {
+    var Phaser = {
+        Rectangle: {
+            intersects: sinon.stub()
+        },
+        Tilemap: {
+            TILED_JSON: 'tiled-json'
+        },
+        ScaleManager: {
+            SHOW_ALL: 'show-all'
+        }
+    };
+    var PhaserGame = {
+        destroy: sinon.stub(),
+        load: {
+            tilemap: sinon.spy(),
+            image: sinon.spy()
+        },
+        add: {
+            tilemap: sinon.stub(),
+            image: sinon.stub()
+        },
+        physics: {
+            startSystem: sinon.spy(),
+            arcade: {
+                enable: sinon.spy()
+            }
+        },
+        scale: {}
+    }, PhaserFactory = {
         newGame: sinon.stub()
     };
 
@@ -91,7 +118,23 @@ describe('Service: gameDetails', function () {
     var service, rootScope;
     var phaserCBs;
     beforeEach(inject(function ($injector, $rootScope) {
+        Phaser.Rectangle.intersects.reset();
         PhaserFactory.newGame.reset();
+        PhaserGame.destroy.reset();
+        PhaserGame.add.image.reset();
+        PhaserGame.add.tilemap.reset();
+        PhaserGame.load.image.reset();
+        PhaserGame.load.tilemap.reset();
+        PhaserGame.physics.startSystem.reset();
+        PhaserGame.physics.arcade.enable.reset();
+        PhaserGame.scale = {
+            scaleMode: undefined,
+            pageAlignHorizontally: undefined,
+            pageAlignVertically: undefined,
+            windowConstraints: {
+                bottom: undefined
+            }
+        };
         phaserCBs = undefined;
         rootScope = $rootScope;
         service = $injector.get('tbsShipGridV2');
@@ -120,6 +163,31 @@ describe('Service: gameDetails', function () {
         it('test initialize gets data from servers and initializes a phaser game', function () {
             expect(phaserCBs).to.be.defined;
         });
+
+        it('stopping game', function () {
+            service.stop();
+            assert(PhaserGame.destroy.calledWithMatch())
+        });
+
+        it('preload phaser sets up phaser', function () {
+            phaserCBs.preload();
+            assert(PhaserGame.load.tilemap.calledWithMatch('grid', 'templates/gamefiles/20x20.json', null, Phaser.Tilemap.TILED_JSON));
+            assert(PhaserGame.load.image.calledWithMatch('tile', 'images/' + expectedTheme + '/tile.png'));
+            assert(PhaserGame.load.image.calledWithMatch('highlight', 'images/' + expectedTheme + '/highlight.png'));
+            assert(PhaserGame.load.image.calledWithMatch('extendedhighlight', 'images/' + expectedTheme + '/extendedhighlight.png'));
+            angular.forEach(expectedShips, function (ship) {
+                assert(PhaserGame.load.image.calledWithMatch(ship.ship, 'images/' + expectedTheme + '/' + angular.lowercase(ship.ship) + '.png'));
+            });
+            angular.forEach(expectedStates, function (state) {
+                var lower = angular.lowercase(state);
+                assert(PhaserGame.load.image.calledWithMatch(lower, 'images/' + expectedTheme + '/' + lower + '.png'));
+            })
+        });
+
+        it('init sets up scale', function () {
+            phaserCBs.init();
+            expect(PhaserGame.scale.scaleMode).to.equal(Phaser.ScaleManager.SHOW_ALL);
+        })
     });
 
 });
