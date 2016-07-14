211d6964b9e43ab7097ad8f5fcf38a8482936533
JTB <jtbdevelopment@gmail.com>
2016-07-13 21:11:08 -0400

    removed the unused default option field

--- game/src/main/groovy/com/jtbdevelopment/TwistedBattleship/state/GameFeature.groovy
+++ game/src/main/groovy/com/jtbdevelopment/TwistedBattleship/state/GameFeature.groovy
@@ -8,20 +8,20 @@ import groovy.transform.CompileStatic
  */
 @CompileStatic
 enum GameFeature {
-    GridSize(1, GameFeatureGroupType.Difficulty, Grid10x10, 'Grid', 'Size of the ocean each player is in.'),
+    GridSize(1, GameFeatureGroupType.Difficulty, 'Grid', 'Size of the ocean each player is in.'),
     Grid10x10(1, '10x10', '10 x 10 square grid', GridSize),
     Grid15x15(2, '15x15', '15 x 15 square grid', GridSize),
     Grid20x20(3, '20x20', '20 x 20 square grid', GridSize),
 
-    ActionsPerTurn(2, GameFeatureGroupType.Difficulty, PerShip, 'Actions Per Turn', 'How many actions can a player take per turn?'),
+    ActionsPerTurn(2, GameFeatureGroupType.Difficulty, 'Actions Per Turn', 'How many actions can a player take per turn?'),
     Single(2, 'Single Action', 'A player can fire a single shot or take a single special action (ECM, Spying, etc) per turn.', ActionsPerTurn),
     PerShip(1, 'Per Ship', 'A player has as many action points as they have ships.  Firing takes 1 action point, specials (ECM, Spying, etc.) take 2.', ActionsPerTurn),
 
-    FogOfWar(3, GameFeatureGroupType.Difficulty, SharedIntel, 'Fog of War', 'How much information is shared between players?'),
+    FogOfWar(3, GameFeatureGroupType.Difficulty, 'Fog of War', 'How much information is shared between players?'),
     SharedIntel(1, 'Shared Intel', 'Players can see the results of all actions taken by all players.', FogOfWar),
     IsolatedIntel(2, 'Isolated Views', 'Each player can only see the impact of their own actions and the defensive actions of other players.', FogOfWar),
 
-    StartingShips(4, GameFeatureGroupType.Difficulty, StandardShips, 'Ships', 'What ships are there?'),
+    StartingShips(4, GameFeatureGroupType.Difficulty, 'Ships', 'What ships are there?'),
     StandardShips(1, 'Standard Ships', 'Players get 5 ships of varying types and sizes.', StartingShips),
     AllCarriers(2, 'All Carriers', 'Each player gets 5 aircraft carriers.', StartingShips),
     AllDestroyers(3, 'All Destroyers', 'Each player gets 5 destroyers.', StartingShips),
@@ -29,23 +29,23 @@ enum GameFeature {
     AllCruisers(5, 'All Cruisers', 'Each player gets 5 cruisers.', StartingShips),
     AllBattleships(6, 'All Battleships', 'Each player gets 5 battleships.', StartingShips),
 
-    ECM(5, GameFeatureGroupType.Defensive, ECMEnabled, 'Electronic Countermeasures', 'ECM devices allow player to scramble opponent views of their ocean.'),
+    ECM(5, GameFeatureGroupType.Defensive, 'Electronic Countermeasures', 'ECM devices allow player to scramble opponent views of their ocean.'),
     ECMEnabled(1, 'Enabled', 'Players can use ECM device to cloak their grid.  Bigger grids hide bigger areas.', ECM),
     ECMDisabled(2, 'Disabled', 'ECM option disabled.', ECM),
 
-    EvasiveManeuvers(6, GameFeatureGroupType.Defensive, EMEnabled, 'Evasive Maneuvers', 'Allows defensive evasive maneuvers of a ship.'),
+    EvasiveManeuvers(6, GameFeatureGroupType.Defensive, 'Evasive Maneuvers', 'Allows defensive evasive maneuvers of a ship.'),
     EMEnabled(1, 'Enabled', 'The captain will take emegency evasive actions, moving a few spaces away and possibly turning 90 degrees.  This also scrambles opponent records of the area somewhat randomly.', EvasiveManeuvers),
     EMDisabled(2, 'Disabled', 'Evasive maneuvers disabled.', EvasiveManeuvers),
 
-    EmergencyRepairs(7, GameFeatureGroupType.Defensive, EREnabled, 'Emergency Repairs', 'Allows a damaged ship to be repaired.'),
+    EmergencyRepairs(7, GameFeatureGroupType.Defensive, 'Emergency Repairs', 'Allows a damaged ship to be repaired.'),
     EREnabled(1, 'Enabled', 'Ship crew are able to salvage parts to repair a ship to full sea-worthiness.', EmergencyRepairs),
     ERDisabled(2, 'Disabled', 'Emergency repairs disabled.', EmergencyRepairs),
 
-    Spy(8, GameFeatureGroupType.Offensive, SpyEnabled, 'Spying', "Allows player's drones to spy on opponents."),
+    Spy(8, GameFeatureGroupType.Offensive, 'Spying', "Allows player's drones to spy on opponents."),
     SpyEnabled(1, 'Enabled', "Players can use spy drones to get a glimpse of an area of an opponent's grid.  Bigger grids show more area.", Spy),
     SpyDisabled(2, 'Disabled', 'Spy drones disabled.', Spy),
 
-    CruiseMissile(9, GameFeatureGroupType.Offensive, CruiseMissileDisabled, 'Cruise Missile', "Single use attack that sinks a ship with a single hit on any location."),
+    CruiseMissile(9, GameFeatureGroupType.Offensive, 'Cruise Missile', "Single use attack that sinks a ship with a single hit on any location."),
     CruiseMissileEnabled(1, 'Enabled', "Single use per game, sinks a ship by hitting any single ship location.", CruiseMissile),
     CruiseMissileDisabled(2, 'Disabled', 'Cruise missile disabled.', CruiseMissile),
 
@@ -53,14 +53,12 @@ enum GameFeature {
     final GameFeature group
     final String label
     final String description
-    final GameFeature groupDefault
     final int order
 
     //  Constructor for groups
     public GameFeature(
             final int order,
             final GameFeatureGroupType groupType,
-            final GameFeature groupDefault,
             final String label,
             final String description
     ) {
@@ -68,7 +66,6 @@ enum GameFeature {
         this.label = label
         this.description = description
         this.group = this
-        this.groupDefault = groupDefault
         this.groupType = groupType
     }
 
@@ -81,7 +78,6 @@ enum GameFeature {
         this.order = order
         this.description = description
         this.group = group
-        this.groupDefault = null
         this.label = label
         this.groupType = group.groupType
     }
