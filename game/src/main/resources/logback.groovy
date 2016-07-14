80b3c9b84ed40f957070222005a9f6224232c1f9
JTB <jtbdevelopment@gmail.com>
2016-07-13 21:20:37 -0400

    silenced annoying cyclc

--- game/src/main/resources/logback.groovy
+++ game/src/main/resources/logback.groovy
@@ -1,3 +1,5 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.status.OnConsoleStatusListener
 
 // always a good idea to add an on console status listener
@@ -9,6 +11,7 @@ appender("CONSOLE", ConsoleAppender) {
     }
 }
 
+logger("org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver", WARN)
 logger("com.jtbdevelopment.games.push", TRACE)
 //logger("com.jtbdevelopment.games.websocket.AtmosphereListener", TRACE)
 root(INFO, ["CONSOLE"])
